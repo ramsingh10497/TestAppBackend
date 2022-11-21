8eb7bc502111631c9d2cfc7522355a82ef857ba1
ramsingh10497 <rams48705@gmail.com>
2022-11-22 03:21:13 +0530

    getting loggedIn user by lat long

--- controllers/authentication/auth.js
+++ controllers/authentication/auth.js
@@ -2,20 +2,80 @@ const authHelper = require("../../utils/auth/auth");
 const helper = require("../../utils/shared");
 const knex = require("../../config/db");
 const { positionPath } = require("../../utils/externalAPIpath");
-const fetch = require("node-fetch")
-require('dotenv').config()
+const fetch = require("node-fetch");
+require("dotenv").config();
 
+// getting Specefic user
 const getUser = async (req, res) => {
   const user = await knex("users").where({ id: req?.params?.id }).first();
   const { password, ...rest } = user;
   return res.status(200).send(rest);
 };
 
-const loggedUsers = (req, res) => {
+// getting logged user by lat and long
+const loggedUsers = async (req, res) => {
   let sessions = req.sessionStore.sessions;
-  res.status(200).send(sessions);
+  const sessionName = Object.keys(sessions);
+  const sessionNameValues = Object.values(sessions).map((item) =>
+    JSON.parse(item)
+  );
+  const currentUser = req.session?.passport?.user;
+
+  const loggedUsers = sessionNameValues.map((item, index) => {
+    if (item?.passport && currentUser?.id !== item.passport.user.id) {
+      const user = item.passport.user;
+      const sessionId = sessionName[index];
+      const { password, ...excludedPasswordUser } = user;
+      return { ...excludedPasswordUser, sessionId };
+    }
+    return;
+  });
+
+  const latitude = currentUser?.lat ?? 28.626137;
+  const longitude = currentUser?.long ?? 79.821602;
+
+  const haversine = `(
+    6371 * acos(
+        cos(radians(${latitude}))
+        * cos(radians(lat))
+        * cos(radians(long) - radians(${longitude}))
+        + sin(radians(${latitude})) * sin(radians(lat))
+    )
+)`;
+
+  const users = await knex("users")
+    .select("id", knex.raw(`${haversine} as distance`))
+    .orderBy("distance", "ASC");
+
+  let sortedLoggedUser = [];
+  let indexes = [];
+
+  for (let index = 0; index < loggedUsers.length; index++) {
+    for (let j = 0; j < users.length; j++) {
+      if (
+        users[j]?.id == loggedUsers[index]?.id &&
+        sortedLoggedUser.length < 5
+      ) {
+        sortedLoggedUser.push(loggedUsers[index]);
+        indexes.push(j);
+      }
+    }
+  }
+
+  for (let i = 0; i < indexes.length; i++) {
+    for (j = i + 1; j < indexes.length - i - 1; j++) {
+      if (indexes[j] > indexes[j + 1]) {
+        var temp = sortedLoggedUser[j];
+        sortedLoggedUser[j] = sortedLoggedUser[j + 1];
+        sortedLoggedUser[j + 1] = temp;
+      }
+    }
+  }
+  // console.log(req.session, "sessions");
+  res.status(200).send(sortedLoggedUser);
 };
 
+// Registering the user
 const userRegister = async (req, res) => {
   const { email, zipcode } = req.body;
   if (!email || !zipcode) {
@@ -23,15 +83,18 @@ const userRegister = async (req, res) => {
   }
 
   let headersList = {
-    "Accept": "*/*"
-   }
-   let response = await fetch(`${positionPath}?access_key=${process.env.POSTIONSTACK_ACCESS_KEY}&query=${zipcode}&limit=1`, { 
-     method: "GET",
-     headers: headersList
-   });
-   let data = await response.json();
-   req.body.lat = data.data[0].latitude ?? 0
-   req.body.long = data.data[0].longitude ?? 0
+    Accept: "*/*",
+  };
+  let response = await fetch(
+    `${positionPath}?access_key=${process.env.POSTIONSTACK_ACCESS_KEY}&query=${zipcode}&limit=1`,
+    {
+      method: "GET",
+      headers: headersList,
+    }
+  );
+  let data = await response.json();
+  req.body.lat = data.data[0]?.latitude ?? 10000;
+  req.body.long = data.data[0]?.longitude ?? 10000;
 
   const user = await knex("users").where({ email: req?.body?.email }).first();
   if (user) {
@@ -59,6 +122,7 @@ const userRegister = async (req, res) => {
   }
 };
 
+// login User
 const userLogin = (req, res) => {
   if (!req.user) {
     helper.handleResponse(res, 401, req.messages);
@@ -70,7 +134,6 @@ const userLogin = (req, res) => {
   };
 
   const token = helper.jwtTokenCreation(payload);
-
   res.status(200).send({
     success: true,
     message: "Logged in Successfully",
@@ -79,6 +142,7 @@ const userLogin = (req, res) => {
   });
 };
 
+// Updating user detail
 const updateUserProfile = async (req, res) => {
   const user = await knex("users").where({ id: req?.params?.id }).first();
   if (user) {
@@ -103,6 +167,7 @@ const updateUserProfile = async (req, res) => {
   return res.status(200).send("success");
 };
 
+// Logout User
 const logoutUser = (req, res) => {
   req.session.destroy();
   res.status(200).send("Logout Successfully");
